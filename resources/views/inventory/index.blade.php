945d55595e85ef58b3af9b63ae11bcf600b6f2b2
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-05-05 14:19:46 +0630

    sort

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -40,7 +40,12 @@
         <thead>
             <tr>
                 <td>#</td>
-                <td>Name</td>
+                <td>
+                    Name
+                    <a class=" btn btn-sm btn-outline-primary" href="{{ route('item.index') }}?name=asc">ASC</a>
+                    <a class=" btn btn-sm btn-outline-primary" href="{{ route('item.index') }}?name=desc">DESC</a>
+                    <a class=" btn btn-sm btn-outline-primary" href="{{ route('item.index') }}">Clear</a>
+                </td>
                 <td>Price</td>
                 <td>Stock</td>
                 <td>Control</td>
